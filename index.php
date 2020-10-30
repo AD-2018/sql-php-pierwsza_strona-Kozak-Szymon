e279baa14e1afcb90f06be1509ff3d6f922fca6f
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-10-30 10:28:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-echo ("<h3> mam na imie Szymon </h3><br>");
-echo ("<h3> mam 18 lat </h3>");
+$servername = "mysql-szymonkozak.alwaysdata.net";
+$username = "217196_jan";
+$password = "Kacpertorudamałpa_12";
+$dbname = "szymonkozak_pracownicy";
+
+$conn = new mysqli ($servername, $username, $password, $dbname);
+
+$sql "SELECT * FROM pracownicy";
+    echo ("<h3> ZADANIE 1 </h3>");
+    echo ("<li>".$sql."<br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }
+    echo ('</table>');
 ?>
