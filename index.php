f5b3a0dc52e7ac5d1068ace4b73538ebeb675cfd
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-05 09:25:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $sql = "SELECT dzial,avg(zarobki) as srednia FROM pracownicy where avg(zarobki)<
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+    echo ("<tr><th>srednia</th><th>dzial</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
                 echo ('<tr>');
                 echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
