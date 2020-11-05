e06b3dfc30047e8486f2c37a48b86a38ee1770f8
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-05 08:55:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,19 @@ $result = mysqli_query($conn, $sql);
                 echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
                 echo ('</tr>');
         }
+   
+    echo ('</table>');
+    echo("<h3> ZAD 2 </h3>");
+$sql = "SELECT * FROM pracownicy where imie like '%a'";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }
     echo ('</table>');
 ?>
     </body>
