a4fa6c8e824c4c7841ed366760c4460143cb8105
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-05 09:17:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -49,19 +49,7 @@ $result = mysqli_query($conn, $sql);
                 echo ('</tr>');
         }echo ('</table>');
     
-           echo("<h3> ZAD 3 </h3>");
-$sql = "SELECT dzial, avg(zarobki) FROM pracownicy where(avg(zarobki)<40) group by dzial;
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>zarobki</th><th>dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ('<td>'.$row["zarobki"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
-        }echo ('</table>');
-    
-        echo("<h3> ZAD 4 </h3>");
+        echo("<h3> ZAD 3 </h3>");
 $sql = "SELECT * FROM pracownicy where imie like '%a'";
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
@@ -71,7 +59,19 @@ $result = mysqli_query($conn, $sql);
                 echo ('<tr>');
                 echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
                 echo ('</tr>');
-        }echo ('</table>');   
+        }echo ('</table>'); 
+    
+          echo("<h3> ZAD 4 </h3>");
+$sql = "SELECT * FROM pracownicy where(dzial=1 or dzial=2)";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
 ?>
     </body>
 </html>
