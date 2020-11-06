9be3c98b2414091c81622d74e2eadfc233999ff4
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-06 10:05:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 <body>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kozak-Szymon">github</a>
     <h1>Szymon Kozak</h1>
+    
+    <div class = "nav">
+        <a href="index.php">strona główna</a>
+        <br>
+        <a href="pracownicy_organizacja.php">pracownicy organizacja</a>
+        <br>
+        <a href="funkcje_agregujace.php">Funkcje Agregujace</a>
+    </div>
 <?php
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
