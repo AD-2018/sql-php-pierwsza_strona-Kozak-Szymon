587170a5d4e464d022e1f591b956f867eba8db6d
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-06 10:25:24 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -75,14 +75,14 @@ $result = mysqli_query($conn, $sql);
     
     
        echo("<h3> ZAD 4 </h3>");
-$sql = "SELECT * FROM pracownicy,organizacja where dzial=nazwa_dzial";
+$sql = "SELECT imie, nazwa_dzial FROM pracownicy,organizacja where id_org=dzial";
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>nazwa_dzial</th></tr>");
+    echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
                 echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["nazwa_dzial"].'</td>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["nazwa_dzial"].'</td>');
                 echo ('</tr>');
         }echo ('</table>');
     
