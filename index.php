266e63da480051bb528f12de0bff440f0054bded
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-06 10:41:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,54 +32,6 @@ $result = mysqli_query($conn, $sql);
                 echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
                 echo ('</tr>');
         }echo ('</table>');
-    
-    echo("<h3> ZAD 1 </h3>");
-$sql = "select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial and imie not like '%a' group by dzial having avg(zarobki)<35";
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ("<td>".$row['avg(zarobki)']."</td><td>".$row['nazwa_dzial']."</td>");
-                echo ('</tr>');
-        }echo ('</table>');
-    
-        echo("<h3> ZAD 2 </h3>");
-$sql = "select avg(zarobki),nazwa_dzial from pracownicy,organizacja where id_org=dzial group by dzial having avg(zarobki)<40";
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>avg(zarobki)</th><th>nazwa_dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ("<td>".$row['avg(zarobki)']."</td><td>".$row['nazwa_dzial']."</td>");
-                echo ('</tr>');
-        }echo ('</table>');
-    
-        echo("<h3> ZAD 3 </h3>");
-$sql = "SELECT * FROM pracownicy where (zarobki<40) and imie not like '%a'";
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
-        }echo ('</table>'); 
-    
-          echo("<h3> ZAD 4 </h3>");
-$sql = "SELECT * FROM pracownicy where(dzial=1 or dzial=2)";
-    echo ("<li>".$sql."</li><br><br>");
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
-        while ($row = mysqli_fetch_assoc($result)) {
-                echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
-        }echo ('</table>');
 ?>
     </body>
 </html>
