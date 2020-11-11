0622dcfab5e0df4d8295628e7dbc88be28858796
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-11 20:03:41 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -90,10 +90,10 @@ $sql = "SELECT  * FROM pracownicy,organizacja WHERE id_org=dzial order by imie d
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>");
+    echo ("<tr><th>imie</th><th>zarobki</th><th>data_urodzenia</th><th>dzial</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
                 echo ('<tr>');
-                echo ('<td>'.$row["imie"].'</td><td>'.$row["nazwa_dzial"].'</td>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
                 echo ('</tr>');
         }echo ('</table>');
     ?>
