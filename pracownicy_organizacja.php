3d26ac558443ec5a686fca5d4ce37f87af96ce29
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-11 21:17:15 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -83,7 +83,7 @@ $result = mysqli_query($conn, $sql);
     
    
    echo("<h3> ZAD 6 </h3>");
-$sql = "SELECT  * FROM pracownicy,organizacja WHERE id_org=dzial order by imie desc;";
+$sql = "SELECT  * FROM pracownicy,organizacja WHERE id_org=dzial order by imie desc";
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
@@ -94,6 +94,17 @@ $result = mysqli_query($conn, $sql);
                 echo ('</tr>');
         }echo ('</table>');
     
+    echo("<h3> ZAD 7 </h3>");
+$sql = "SELECT  * FROM pracownicy,organizacja WHERE id_org=dzial order by imie asc";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["nazwa_dzial"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
     
     ?>
 </body>
