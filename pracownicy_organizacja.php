1c03481d353c476f350837950dd055e04f55041d
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-11 21:00:09 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -58,7 +58,7 @@ $result = mysqli_query($conn, $sql);
         }echo ('</table>');
     
     echo("<h3> ZAD 3 </h3>");
-$sql = "SELECT avg(zarobki) FROM pracownicy WHERE imie NOT LIKE '%a' and (dzial=1 OR dzial=2)";
+$sql = "SELECT  * FROM pracownicy,organizacja WHERE dzial = nazwa_dzial and dzial=1 and dzial=4";
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
