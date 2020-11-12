f2858bdf9a82efc63dbba481462e9effc0ef29e6
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-12 09:03:04 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -34,6 +34,19 @@ $result = mysqli_query($conn, $sql);
                 echo ('<td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
                 echo ('</tr>');
         }echo ('</table>');
+    
+    echo("<h3> ZAD 1</h3>");
+$sql = "select *,DATEDIFF(CURDATE(),data_urodzenia) AS wiek from pracownicy,organizacja where id_org=dzial";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo ("<tr><th>imie</th><th>dzial</th><th>wiek</th></tr>");
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                echo ('<td>'.$row["imie"].'</td><td>'.$row["nazwa_dzial"].'</td><td>'.$row["wiek"].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
+    
         
         ?>
         </body>
