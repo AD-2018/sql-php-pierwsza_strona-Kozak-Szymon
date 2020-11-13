170884797ded41e063749bc521226fa752ca393f
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-13 11:24:06 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -256,6 +256,18 @@ $result = mysqli_query($conn, $sql);
                 echo ('</tr>');
         }echo ('</table>');
     
+    echo("<h3> ZAD 8 </h3>");
+$sql = "SELECT DATE_FORMAT("2002-05-16", '%j') as urodzenia";
+    echo ("<li>".$sql."</li><br><br>");
+$result = mysqli_query($conn, $sql);
+    echo ('<table border = "1" class = "moja_tabelka">');
+    echo('<th>Data</th>');
+        while ($row = mysqli_fetch_assoc($result)) {
+                echo ('<tr>');
+                 echo('<td>'.$row['urodzenia'].'</td>');
+                echo ('</tr>');
+        }echo ('</table>');
+    
    echo("<h3> ZAD 8 </h3>");
 $sql = "SELECT DATE_FORMAT(data_urodzenia,'%W') as dzien, imie, data_urodzenia FROM pracownicy ORDER BY CASE
           WHEN dzien = 'Poniedziałek' THEN 1
