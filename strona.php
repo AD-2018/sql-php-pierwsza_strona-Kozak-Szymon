92468ac9c7a25a1a9ba3a4f585ca7a2f7e5c446f
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-19 09:02:37 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,3 +1,18 @@
+<div class = "nav">
+        <a href="index.php">strona główna</a>
+        <br>
+        <a href="pracownicy_organizacja.php">pracownicy organizacja</a>
+        <br>
+        <a href="funkcje_agregujace.php">Funkcje Agregujace</a>
+        <br>
+        <a href="data_czas.php">Data i Czas</a>
+        <br>
+        <a href="Formularz.html">Formularze</a>
+        <br>
+        <a href="strona.php">Strona Formularza</a>
+    </div>
+
+
 <?php
 	echo("jesteś na stronie.php");
 	
