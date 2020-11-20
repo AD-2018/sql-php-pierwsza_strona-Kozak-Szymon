091d8baeab8daf34c1e193060e972c4bd62b2d39
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-20 12:27:09 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,11 @@
 <?php
+echo("jestes w insert.php");
+echo "<li>".$_POST['imie'];
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['zarobki'];
+echo "<li>".$_POST['data_urodzenia'];
+
+
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
 $password = "Kacpertorudamałpa_12";
@@ -10,9 +17,13 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null, 'Ksawery', 1, 76,'1991-11-21')";
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
-$conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
