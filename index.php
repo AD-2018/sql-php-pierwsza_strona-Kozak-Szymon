49630d192594ab744a8111a17483031438c33328
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-20 12:30:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <br>
         <a href="strona.php">Strona Formularza</a>
         <br>
-        <a href="dane_do_bazy.php">Dane do bazy</a>
+        <a href="dane_do_bazy.html">Dane do bazy</a>
         <br>
         <a href="insert.php">insert</a>
     </div>
