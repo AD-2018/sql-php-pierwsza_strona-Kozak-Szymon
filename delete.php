f3ea36d4ee6e7538f14009fc3a8e590c2571bd19
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-20 12:57:59 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kozak-Szymon">github</a>
+    <h1>Szymon Kozak</h1>
+    
+   <div class = "nav">
+        <a href="index.php">strona główna</a>
+        <br>
+        <a href="pracownicy_organizacja.php">pracownicy organizacja</a>
+        <br>
+        <a href="funkcje_agregujace.php">Funkcje Agregujace</a>
+        <br>
+        <a href="data_czas.php">Data i Czas</a>
+        <br> 
+        <a href="Formularz.html">Formularze</a>
+        <br>
+        <a href="dane_do_bazy.html">Dane do bazy</a>
+    </div>
 
+
+<?php
+echo ("<h3>jesteś w delete.php<h3>";
+?>
