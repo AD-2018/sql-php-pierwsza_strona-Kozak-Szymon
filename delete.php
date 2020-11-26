519e9aa20432c02206b97f170e381fbe9dae195a
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-26 08:19:51 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,29 +1,28 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-Kozak-Szymon">github</a>
-    <h1>Szymon Kozak</h1>
-    
-   <div class = "nav">
-        <a href="index.php">strona główna</a>
-        <br>
-        <a href="pracownicy_organizacja.php">pracownicy organizacja</a>
-        <br>
-        <a href="funkcje_agregujace.php">Funkcje Agregujace</a>
-        <br>
-        <a href="data_czas.php">Data i Czas</a>
-        <br> 
-        <a href="Formularz.html">Formularze</a>
-        <br>
-        <a href="dane_do_bazy.html">Dane do bazy</a>
-    </div>
-
-
 <?php
-echo ("<h3>jesteś w delete.php<h3>";
+echo("Delete<br>");
+echo $_POST['id'];
+
+$servername = "mysql-szymonkozak.alwaysdata.net";
+$username = "217196_jan";
+$password = "Kacpertorudamałpa_12";
+$dbname = "szymonkozak_pracownicy";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM Pracownik WHERE id= $_POST['id'];";
+
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
-</body>
-</html>
