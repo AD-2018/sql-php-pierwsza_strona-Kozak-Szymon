4952432908fc8ec5e4f5cb00a995b282210c16d1
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-26 08:54:58 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+echo("jestes w delete.php <br>");
+echo("$_POST['id']");
+
+$servername = "mysql-szymonkozak.alwaysdata.net";
+$username = "217196_jan";
+$password = "Kacpertorudamałpa_12";
+$dbname = "szymonkozak_pracownicy";
+
+$conn = new mysqli ($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM Pracownicy WHERE id=".$_POST['id'];
+echo("$sql");
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
