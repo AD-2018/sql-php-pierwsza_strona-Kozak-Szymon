077bd3d9510ee58c72ccbeabbc7d76c407d1be8a
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 11:12:57 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -15,15 +15,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
+//definiujemy zapytanie $sql
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
-
+//wyświetlamy zapytanie $sql
 echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
-   echo "New record created successfully";
+  echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
