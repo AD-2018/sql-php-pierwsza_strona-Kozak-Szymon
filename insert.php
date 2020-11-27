48d98a33b44f9688305fdbc858024a3256242246
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 10:42:13 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,5 +1,9 @@
 <?php
-echo("Insert");
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['imie'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
@@ -11,9 +15,12 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
-       VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
-echo "<li>".$sql;
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
