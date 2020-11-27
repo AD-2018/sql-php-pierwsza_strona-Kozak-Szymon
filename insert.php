30a25323df9b370d5b102ae6783e4349f996ebc9
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 10:47:15 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -15,15 +15,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//definiujemy zapytanie $sql
+
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
-//wyświetlamy zapytanie $sql
+
 echo "<li>". $sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  header ('Location: https://kozak-szymon.herokuapp.com/dane_do_bazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
