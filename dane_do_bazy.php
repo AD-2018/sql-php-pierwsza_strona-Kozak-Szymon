aacd98b871d8a058879a3bd18a599f6284e4b95e
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 10:22:02 +0100

    Update dane_do_bazy.php

--- dane_do_bazy.php
+++ dane_do_bazy.php
@@ -51,7 +51,7 @@ $sql = "SELECT * FROM pracownicy";
     echo ("<li>".$sql."</li><br><br>");
 $result = mysqli_query($conn, $sql);
     echo ('<table border = "1" class = "moja_tabelka">');
-    echo ("<tr><th>ID</th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th></tr>");
+    echo ("<tr><th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
                 echo ('<tr>');
                 echo ('<td>'.$row["id_pracownicy"].'</td><td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
