b8968640d7f27167703a3c375f2b36b2bea4c90f
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 10:25:25 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,29 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
-echo("jestes w insert.php");
-echo "<li>".$_POST['imie'];
-echo "<li>".$_POST['dzial'];
-echo "<li>".$_POST['zarobki'];
-echo "<li>".$_POST['data_urodzenia'];
-
-
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
 $password = "Kacpertorudamałpa_12";
 $dbname = "szymonkozak_pracownicy";
 
-$conn = new mysqli ($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 $sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
-       VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')";
+       VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
+
 
 if ($conn->query($sql) === TRUE) {
-  header ('Location: https://kozak-szymon.herokuapp.com/dane_do_bazy.php');
+  header('https://kozak-szymon.herokuapp.com/dane_do_bazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
 ?>
+</body>
+</html>
