7dbc58f2c4a0689a284d239c0b867d309dc36e2a
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-11-27 10:29:31 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,15 +1,22 @@
 <?php
+echo("Insert");
+
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
 $password = "Kacpertorudamałpa_12";
 $dbname = "szymonkozak_pracownicy";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 $sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
        VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
-
+echo "<li>".$sql;
 
 if ($conn->query($sql) === TRUE) {
-  echo ("mamy mózg");
+  echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
