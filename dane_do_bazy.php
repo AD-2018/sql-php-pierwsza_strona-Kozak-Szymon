8675687f136fd0292f3403af48398c68d44d5413
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-12-03 08:50:40 +0100

    Update dane_do_bazy.php

--- dane_do_bazy.php
+++ dane_do_bazy.php
@@ -52,8 +52,14 @@ $result = mysqli_query($conn, $sql);
     echo ("<tr><th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
                 echo ('<tr>');
-                echo ('<td>'.$row["id_pracownicy"].'</td><td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>');
-                echo ('</tr>');
+                echo ('<td>'.$row["id_pracownicy"].'</td><td>'.$row["imie"].'</td><td>'.$row["zarobki"].'</td><td>'.$row["data_urodzenia"].'</td><td>'.$row["dzial"].'</td>'
+		'<td>.
+	  <form action="delete.php" method="POST">
+          <input type="hidden" name="id" value="'.$row['id_pracownicy'].'"></br>
+          <input type="submit" value="Usuń">
+    	  </form>);
+                echo ('</tr>'
+		</td>');
         }echo ('</table>');
 ?>
 </body>
