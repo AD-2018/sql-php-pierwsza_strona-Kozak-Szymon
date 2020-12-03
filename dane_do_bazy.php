ac2aaa53e1325a35792bfae98902fef90a620a9a
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2020-12-03 09:12:32 +0100

    Update dane_do_bazy.php

--- dane_do_bazy.php
+++ dane_do_bazy.php
@@ -57,7 +57,7 @@ $result = mysqli_query($conn, $sql);
 	'<td>
 	
 	  <form action="delete.php" method="POST">
-          <input type="yext" name="id_pracownicy" value="'.$row["id_pracownicy"].'" hidden>
+          <input type="text" name="id_pracownicy" value="'.$row["id_pracownicy"].'" hidden>
           <input type="submit" value="Usun">
     	  </form>
 	  </td>');
