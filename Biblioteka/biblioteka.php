bccafc629b1d0518ed261b52262ebe34cbaa5ede
Szymon Kozak <szymon.kozak98760@gmail.com>
2021-02-04 13:57:36 +0100

    proba 2

--- Biblioteka/biblioteka.php
+++ Biblioteka/biblioteka.php
@@ -24,6 +24,20 @@
         <a href="/Biblioteka/biblioteka.php">biblioteka</a>
 </div>
 
+<h3>DODAJ TYTUL</h3>
+    <form action="/Biblioteka/new_tytul.php" method="POST">
+    	<p>Tytul:</p>
+        <input type="text" name="tytul"></br>
+        <input type="submit" value="dodaj tytul">
+    </form>
+
+    <h3>DODAJ AUTORA</h3>
+    <form action="/Biblioteka/new_autor.php" method="POST">
+    	<p>Tytul:</p>
+        <input type="text" name="autor"></br>
+        <input type="submit" value="dodaj autora">
+    </form>
+
 <?php   
 $servername = "mysql-szymonkozak.alwaysdata.net";
 $username = "217196_jan";
---
+++ Biblioteka/new_autor.php
@@ -0,0 +1,29 @@
+<?php
+echo("jestes w new_autor.php <br>");
+echo "<li>". $_POST['autor'];
+
+$servername = "mysql-szymonkozak.alwaysdata.net";
+$username = "217196_jan";
+$password = "Kacpertorudamałpa_12";
+$dbname = "szymonkozak_pracownicy";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_tytul (id_autor,autor) 
+       VALUES (null, '".$_POST['autor']."')";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+   header ('Location:https://kozak-szymon.herokuapp.com/Biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
