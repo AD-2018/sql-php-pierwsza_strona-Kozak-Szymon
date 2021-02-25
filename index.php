014bdf025f8ec158e83d092afc7b21def63165be
Szymon Kozak <szymon.kozak98760@gmail.com>
2021-02-25 15:29:25 +0100

    localhost

--- index.php
+++ index.php
@@ -22,6 +22,12 @@
     </div>
 <?php   
 
+if ($hostname == 'localhost:') {
+    require_once ("config.php");
+}
+echo("<li> hostname : ".$hostname);
+echo("<li> SERVER passsword: ".$_SERVER['password']);
+
 require_once("connect.php");
 
 $conn = new mysqli ($servername, $username, $password, $dbname);
