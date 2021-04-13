adec298891e0bca7a32e656cb340a15c87316177
Kozak-Szymon <szymon.kozak98760@gmail.com>
2021-04-13 14:33:35 +0200

    chyba dziala

--- cssgrid/HTML1/index.php
+++ cssgrid/HTML1/index.php
@@ -16,34 +16,75 @@
 
         $result = mysqli_query($conn, $sql);
         if ( $result) {
-        echo "<li>ok";
         } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
-
         echo('<table border="1">');
         echo('<th>ID</th><th>Sprawa</th>');
-
         while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
         echo('<td>'.$row['ID'].'</td><td>'.$row['sprawa'].'</td>');
         echo('</tr>');
         }
-
-    echo('</table>');
+        echo('</table>');
                 ?>
             </div>
             <div class="ww">
                 1
             </div>
             <div class="qq">
-                2
+            <?php
+require_once ("../lib.php");
+
+      echo("<br>PRAWNICY I SPRAWY<br>");
+$sql = "SELECT id_sad, Imie, sprawa FROM prawnicy, sprawy, sad WHERE prawnicy.ID = sad.id_prawnik AND sprawy.ID = sad.id_sprawa";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>ID</th><th>PRAWNIK</th><th>SPRAWA</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_sad'].'</td><td>'.$row['Imie'].'</td><td>'.$row['sprawa'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
+
+?>
             </div>
             <div class="ee">
                 4
             </div>
             <div class="yy">
-                3
+            <?php
+               require_once("../../connect.php");
+
+               echo("<br>SPRAWY<br>");
+            $sql = "SELECT * FROM sprawy";
+    echo($sql);
+
+        $result = mysqli_query($conn, $sql);
+        if ( $result) {
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        echo('<table border="1">');
+        echo('<th>ID</th><th>Sprawa</th>');
+        while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['ID'].'</td><td>'.$row['sprawa'].'</td>');
+        echo('</tr>');
+        }
+        echo('</table>');
+                ?>
             </div>
         </div>
     </body>
