4e117176ca7f34621c8a7f11bdf2c7d0b0ae4e29
Kozak-Szymon <szymon.kozak98760@gmail.com>
2021-04-13 14:30:23 +0200

    kurwa no nie

--- cssgrid/HTML1/index.php
+++ cssgrid/HTML1/index.php
@@ -8,23 +8,23 @@
         <div class="container">
             <div class="hh">
                <?php
-               require_once("../connect.php");
+               require_once("../../connect.php");
 
                echo("<br>SPRAWY<br>");
-$sql = "SELECT * FROM sprawy";
-echo($sql);
+            $sql = "SELECT * FROM sprawy";
+    echo($sql);
 
-$result = mysqli_query($conn, $sql);
-if ( $result) {
+        $result = mysqli_query($conn, $sql);
+        if ( $result) {
         echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
 
-echo('<table border="1">');
-    echo('<th>ID</th><th>Sprawa</th>');
+        echo('<table border="1">');
+        echo('<th>ID</th><th>Sprawa</th>');
 
-    while($row=mysqli_fetch_assoc($result)){
+        while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
         echo('<td>'.$row['ID'].'</td><td>'.$row['sprawa'].'</td>');
         echo('</tr>');
