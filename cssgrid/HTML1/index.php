c094991bd4125d5702528289c8557f6b12240c07
Kozak-Szymon <szymon.kozak98760@gmail.com>
2021-04-13 13:47:59 +0200

    poprawki bledow

--- cssgrid/HTML1/index.php
+++ cssgrid/HTML1/index.php
@@ -9,12 +9,10 @@
             <div class="hh">
                <?php
                require_once("../connect.php");
-    
-               $conn = new mysqli ($servername, $username, $password, $dbname);
 
                $sql = "SELECT * FROM Producent";
-$result = mysqli_query($conn, $sql);
-    echo ('<table border = "1" class = "moja_tabelka">');
+               $result = mysqli_query($conn, $sql);
+               echo ('<table border = "1" class = "moja_tabelka">');
     echo ("<tr><th>id_producent</th><th>producent</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
             echo ('<tr>');
