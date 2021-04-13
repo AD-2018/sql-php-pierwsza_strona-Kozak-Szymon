8b9ca436f4269372a392f2f8e2bf10ba6e77d5f5
Kozak-Szymon <szymon.kozak98760@gmail.com>
2021-04-13 13:55:47 +0200

    gsdgsgdsgd

--- cssgrid/HTML1/index.php
+++ cssgrid/HTML1/index.php
@@ -10,28 +10,28 @@
                <?php
                require_once("../connect.php");
 
-               echo("<br>PRODUCENCI<br>");
-               $sql = "SELECT * FROM Producent";
-               echo($sql);
-               
-               $result = mysqli_query($conn, $sql);
-               if ( $result) {
-                       echo "<li>ok";
-                   } else {
-                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                   }
-               
-               echo('<table border="1">');
-                   echo('<th>id_producent</th><th>producent</th>');
-               
-                   while($row=mysqli_fetch_assoc($result)){
-                       echo('<tr>');
-                       echo('<td>'.$row['id_producent'].'</td><td>'.$row['producent'].'</td>');
-                       echo('</tr>');
-                   }
-               
-               echo('</table>');
-               ?>
+               echo("<br>SPRAWY<br>");
+$sql = "SELECT * FROM sprawy";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>ID</th><th>Sprawa</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['ID'].'</td><td>'.$row['sprawa'].'</td>');
+        echo('</tr>');
+        }
+
+    echo('</table>');
+                ?>
             </div>
             <div class="ww">
                 1
