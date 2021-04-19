49ff4dd80a5b3545004f74ebc881a40c63213f9a
Kozak-Szymon <szymon.kozak98760@gmail.com>
2021-04-19 08:22:34 +0200

    menu próba 1

--- index.php
+++ index.php
@@ -10,17 +10,7 @@
 <h1>Szymon Kozak</h1>
 </div>
     <div class = "nav">
-        <a class="panel" href="/pracownicy/pracownicy_organizacja.php">pracownicy organizacja</a>
-        <br>
-        <a class="panel" href="/pracownicy/funkcje_agregujace.php">Funkcje Agregujace</a>
-        <br>
-        <a class="panel" href="/pracownicy/data_czas.php">Data i Czas</a>
-         <br> 
-        <a class="panel" href="/pracownicy/dane_do_bazy.php">Dane do bazy</a>
-        <br>
-        <a class="panel" href="/Biblioteka/biblioteka.php">Biblioteka</a>
-         <br>
-        <a class="panel" href="/cssgrid/index.html">CSSGRID</a>
+    <?php include("menu.php"); ?>
     </div>
 <?php   
 
---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php 
+<a class="panel" href="/pracownicy/pracownicy_organizacja.php">pracownicy organizacja</a>
+<br>
+<a class="panel" href="/pracownicy/funkcje_agregujace.php">Funkcje Agregujace</a>
+<br>
+<a class="panel" href="/pracownicy/data_czas.php">Data i Czas</a>
+ <br> 
+<a class="panel" href="/pracownicy/dane_do_bazy.php">Dodawanie i usuwanie</a>
+<br>
+<a class="panel" href="/Biblioteka/biblioteka.php">Biblioteka</a>
+ <br>
+<a class="panel" href="/cssgrid/index.html">CSSGRID</a>
+?>
