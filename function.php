fa5a5deda5d890763e751db509d72acfb514544d
Szymon Kozak <73634561+Kozak-Szymon@users.noreply.github.com>
2021-04-22 14:20:17 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,3 +1,32 @@
 <?php 
-echo("hello world");
+function selectColumn_werEDU($tableName, $myColumns=[]){
+    print_r($mycolumns);
+    echo ($myColumns[0]);
+    echo ($mycolumns[1]);
+
+    requiere("../../connect.php");
+
+    $kolumny_show=implode(',', $myColumns);
+    echo $kolumny_show;
+    $kolumny = implode(',', $myColumns);
+    echo ("<li>wygenerowany SELECT: "."<br>");
+    $sql="SELECT".$kolumny."FROM".$tableName;
+
+    echo $sql;
+    $result = $conn->query($sql);
+
+    echo("<p><table border=1>");
+    echo("<tr>");
+    for($i=0; $i<count($mycolumns);$i++){
+        echo("<th>".$mycolumns[$i]."</th>");
+    }
+    while($row = $result->fetch_assoc() ){
+        echo("<tr>");
+        for($i=0; $i<count($mycolumns); $i++) {
+            echo("<td>".$row[$mycolumns[$i]]."</td>");
+        }
+        echo("</tr>");
+    }
+    echo("</table></p>");
+}
 ?>
